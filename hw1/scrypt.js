bb30c8ed7bbc62f499e78b1b97df7b99a4596afb
Olka-Khmelova <khmeleva_olga@ukr.net>
2022-12-11 15:42:54 +0200

    Improved some styles of code

--- hw1/scrypt.js
+++ hw1/scrypt.js
@@ -1,56 +1,68 @@
 // 1
 //Создать цикл на 10 итераций. На каждой итерации если i четное, то вывести в консоль слово Fiz, если i не четное, то вывести в консоль слово Buz, если i кротное цифре 3, то вывести FizBuz.
 
-for (let i = 1; i <= 10; i++){
+for (let i = 1; i <= 10; i++) {
     //checking for numbers that divide by 3 without remainder
-    if(i % 3 === 0){
+    if (i % 3 === 0){
         console.log('FizBuz');
     }
     //checking for even numbers
-    else if(i % 2 === 0){
+    else if (i % 2 === 0) {
         console.log('Fiz');
     }
     //checking for odd numbers
-    else if(i % 2 === 1){
+    else if (i % 2 === 1) {
         console.log('Buz');
     }
- 
 }
 // 2
 // Написать логику нахождения факториала числа 10.
 let factorialNumber = 10;
-for(let i = factorialNumber-1; i>=1; i--){
-factorialNumber *= i;
+
+for (let i = factorialNumber - 1; i >= 1; i--) {
+  factorialNumber *= i;
 }
+
 console.log(factorialNumber);
 
 // 3
-// В пачке бумаги 500 листов. За неделю в офисе расходуется 1200 листов. Какое наименьшее количество пачек бумаги нужно купить в офис на 8 недель?
+// В пачке бумаги 500 листов. За неделю в офисе расходуется 1200 листов. 
+// Какое наименьшее количество пачек бумаги нужно купить в офис на 8 недель?
+
 const sheetsInBox = 500;
 const officeNeeds = 1200;
 let period = 8;
-let officeNeedsForPeriod = officeNeeds*period/sheetsInBox;
+
+let officeNeedsForPeriod = officeNeeds * period / sheetsInBox;
+
 console.log(officeNeedsForPeriod.toFixed());
 
 // 4*
-// Создать функцию, которая выведет в консоль номер этажа и номер подъезда по номеру квартиры. Этажей у нас 9, квартир на этаже по 3
+// Создать функцию, которая выведет в консоль номер этажа и номер подъезда по номеру квартиры. 
+//Этажей у нас 9, квартир на этаже по 3.
+
 const roomsOnFloor = 3;
 const floors = 9;
 const roomNumber = 456;
 
-function findPorchAndFloor(roomNumber, floors, roomsOnFloor){
-let porch= (roomNumber-1)/(roomsOnFloor*floors);
-let floor =((roomNumber-1) % (roomsOnFloor*floors))/ roomsOnFloor;
-
-console.log (`The room number ${roomNumber} is on ${floor.toFixed()}th floor, in ${porch.toFixed()}th porch.`);
-}
 findPorchAndFloor(roomNumber, floors, roomsOnFloor);
 
+function findPorchAndFloor(roomNumber, floors, roomsOnFloor) {
 
-// 5**
-// Вывести в консоль пирамиду. Переменная указывает количество строк из которых построится пирамида. Пирамида должна строится в одинаковом визуально виде между собой, но строго учитывая кол-во строк
+  let porch = (roomNumber-1)/(roomsOnFloor*floors);
+  let floor = ( (roomNumber-1) % (roomsOnFloor*floors) ) / roomsOnFloor;
+
+  console.log (`The room number ${roomNumber} is on ${floor.toFixed()}th floor, in ${porch.toFixed()}th porch.`);
+
+}
+
+//  5**
+// Вывести в консоль пирамиду. Переменная указывает количество строк из которых построится пирамида.
+// Пирамида должна строится в одинаковом визуально виде между собой, но строго учитывая кол-во строк.
 
 const medianNumber = 6;
+
+showPyramid(medianNumber);
 
 function showPyramid(n) { 
     //displaying row
@@ -66,10 +78,11 @@ function showPyramid(n) {
       } 
       // displaying '-' after '#' in column of every row
       for (let col = 1; col < n-row; col++) { 
-        string +='-'; 
+        string += '-'; 
      }
+
      console.log(string);  
     } 
-
   } 
-  showPyramid(medianNumber);
+
+
