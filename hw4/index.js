57fdbfa3c97a8a562ccb388858c77cf1ab802891
Olka-Khmelova <khmeleva_olga@ukr.net>
2023-01-11 07:22:12 +0200

    Improved exersice 1

--- hw4/index.js
+++ hw4/index.js
@@ -1,95 +1,91 @@
-let ID = 1;
+/*!!!!!!!!!!!!Exercise 1!!!!!!!!!!!!*/
+
 class Student {
-    constructor(enrollee){
-        this.id = ID++;
-        this.name = enrollee.name;
-        this.surname = enrollee.surname;
-        this.ratingPoint = enrollee.ratingPoint;
-        this.schoolPoint = enrollee.schoolPoint;
-        this.course = enrollee.course;
-        this.isSelfPayment = null;
+    constructor(enrollee) {
+        this.id = Student.ID++;
+        this.isSelfPayment = false;
+
+        Object.assign(this, enrollee);
+
+        Student.listOfStudents.push(this);
+        Student.filterSelfPaymantStudents();
     }
-}
-const createStudentsList = (enrolleesList) => {
-    const listOfStudents= [];
- 
-    for(const enrollee of enrolleesList) {
-       const student = new Student(enrollee);
-       listOfStudents.push(student);
-    }
- 
-    return listOfStudents;
- } 
- 
- const students = createStudentsList(studentArr);
 
- 
- const sortedStudentsList = (array) => {
-    const MAX_RATING_POINTS = 800;
-  
-    for (let i = 0; i < array.length; i++) {
-        // number to compare with
-      for (let j = i + 1; j < array.length; j++) {
+    static ID = 1;
 
-        // comparing 
-        if (array[i].ratingPoint > array[j].ratingPoint && array[i].ratingPoint >= MAX_RATING_POINTS) {
+    static listOfStudents= [];
 
-          let numberToReplace = array[i];
-          //replacing numbers in array
-          array[i] = array[j];
-          array[j] = numberToReplace;
-        }
-        if (array[i].ratingPoint === array[j].ratingPoint && array[i].ratingPoint >= MAX_RATING_POINTS) {
-            if(array[i].schoolPoint > array[j].schoolPoint){
-                let numberToReplace = array[i];
-                //replacing numbers in array
-                array[i] = array[j];
-                array[j] = numberToReplace;
+    static filterSelfPaymantStudents () {
 
+        const BUDGET_PLACES = 5;
+        const MAX_RATING_POINTS = 800;
+
+        const studentsList = Student.listOfStudents;
+
+        for (const index in studentsList) {
+            const student = studentsList[index];
+
+            if (studentsList.length <= 5) {
+                student.isSelfPayment = student.ratingPoint > MAX_RATING_POINTS;
             }
-            Student.isSelfPayment = false;
+            if (studentsList.length > 5) {
+                //sorting students due to rating points
+                for (let i = studentsList.length-1; i >= 0; i--) {
+                // student to compare with
+                    for (let j = i - 1; j >= 0; j--) {
+                // comparing 
+                        if (studentsList[i].ratingPoint > studentsList[j].ratingPoint && studentsList[i].ratingPoint >= MAX_RATING_POINTS) {
+
+                        let numberToReplace = studentsList[i];
+                        //replacing students in array
+                        studentsList[i] = studentsList[j];
+                        studentsList[j] = numberToReplace;
+                        }
+
+                        if (studentsList[i].ratingPoint === studentsList[j].ratingPoint) {
+                        //sorting students due to school points
+                            if(studentsList[i].schoolPoint > studentsList[j].schoolPoint){
+
+                                let numberToReplace = studentsList[i];
+                                //replacing students in array
+                                studentsList[i] = studentsList[j];
+                                studentsList[j] = numberToReplace;
+                            }
+                        }
+                    }
+                }
+            }
+            student.isSelfPayment = index > (BUDGET_PLACES - 1);
         }
-      }
-      }
-console.table(array);
     }
- 
- const filterBudgetStudents = (students) => {
-    let budgetStudentsList = [];
-    for(let i = students.length-1; i < budgetStudentsList.length; i--) {
-        budgetStudentsList.push([i]);
-    }
-    console.log(budgetStudentsList);
 }
 
+for (const enrollee of studentArr) {
+    new Student(enrollee);
+}
+console.table(Student.listOfStudents);
 
- 
-// // filterBudgetStudents();
-// filterBudgetStudents(sortedStudentsList(students));
-// console.table (students);
-sortedStudentsList(students);
- console.table(students);
 
  /*!!!!!!!!!!!!Exercise 2!!!!!!!!!!!!*/
 
 class CustomString {
-    reverse(str) {
-        console.log (str.split("").reverse().join(""));
+
+   reverse(value) {
+        console.log(value.split("").reverse().join(""));
     }
-    ucFirst(str) {
-        console.log(str.charAt(0).toUpperCase()+str.slice(1));
+  ucFirst(value) {
+        console.log(value.charAt(0).toUpperCase() + value.slice(1));
     }
-    ucWords(str) {
-        let splitStr = str.toLowerCase().split(' ');
-        for (var i = 0; i < splitStr.length; i++) {
-       // You do not need to check if i is larger than splitStr length, as your for does that for you
-       // Assign it back to the array
+    ucWords(value) {
+        let splitStr =value.toLowerCase().split(' ');
+
+        for (let i = 0; i < splitStr.length; i++) {
+       //back to the array
             splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
         }
-           // Directly return the joined string
+           //return the joined string
    console.log (splitStr.join(' ')); 
     }
-
 }
 const myString = new CustomString();
 myString.reverse('qwerty'); //выведет 'ytrewq'
@@ -99,31 +95,32 @@ myString.ucWords('qwerty qwerty qwerty'); //выведет 'Qwerty Qwerty Qwerty
  /*!!!!!!!!!!!!Exercise 3!!!!!!!!!!!!*/
 
  class Validator {
-
+    
     checkIsEmail(value) {
-        if(value.length >2 && value.match('@gmail')){
-            console.log (true);
+        if(value.length > 2 && value.match('@gmail')){
+            console.log(true);
         }
     }
     checkIsDomain(value) {
-        if(value.length >2 && value.match('google.com')){
+        if(value.length > 2 && value.match('google.com')){
             console.log (true);
         }
     }
-    checkIsDate(value) {
+     checkIsDate(value) {
         if(value.length === 10 && value.substring(2,3) === '.' && value.substring(5,6) === '.'){
             console.log (true);
         }
     }
     checkIsPhone(value) {
-        if(value.length > 10 && value.substring(0,3) === '+38'){
-            console.log (true);
+        if(value.length > 10 && value.substring(0,3) === '+38'){;
+        console.log (true);
         }
         else{
-            console.log (false); 
+        console.log (false); 
     }
 }
- }
+}
+ 
 const validator = new Validator();
 
 
